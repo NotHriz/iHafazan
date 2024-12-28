2a27ff02e983c9302ac72a370088f8521508aa60
zaim33 <zaimhakimilalisa@gmail.com>
2024-12-28 12:44:00 +0800

    add surah management

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         StudentDetails student = new StudentDetails();
         Scanner scan = new Scanner(System.in);
         boolean login_sucessfull = false;
-    
+
         while (true) {
             try {
                 // Display the menu
